7ecfc52ea08f726a8a8de677d8ed7d7923cfd289
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-15 10:07:18 -0400

    added info for objects

--- objects.js
+++ objects.js
@@ -3,5 +3,13 @@ Object things
 MDN Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
 */
 
+/*
+Objects are super important
+
+During this curriculum the data you will be working with will be stored as objects
+
+Additionally objects are always used to organize data and will be incredibly useful when doing data structures and algorithms.  They are used so frequently they will definitely come up in your interviews
+*/
+
 
 
