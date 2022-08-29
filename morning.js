5bcbffe4b34156198c7cacf168ca8b2eb8b8b221
Rachel Katz <rachel.katz@flatironschool.com>
2022-08-29 15:51:31 -0400

    pasting from github

--- morning.js
+++ morning.js
@@ -64,82 +64,105 @@ let primeTestOne = primeTest.filter((el) => {
 
 
 /*
-Phase 1 Week 1 Day 2
-1. Given a positive integer n return the nth number in the fibonacci sequence. 
-   Where the first two elements in the sequence are 0 and 1
-    Ex. fib(9) => 21
-2. Given an array of strings filter out all strings with unique characters 
-    Ex. ['banana', 'cat', 'apple', 'dog] => ['cat', 'dog']
+Phase 2 Week 1 Day 2
+    1. Function to print nth number in the fibonacci sequence. Ex. fib(9) =>  21
+    2. Given an array of strings filter out all strings with unique characters Ex. ['banana', 'cat', 'apple', 'dog] => ['cat', 'dog']
 */
-const uniqueTest = ['banana', 'cat', 'apple', 'dog']
-//~~~~~~~~~~~~~~~~~~~~~~~~1. fib
+
+//1. fib
 const fib = function(n){
-    //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
-    //sum of previous two numbers 
-    //edge cases (non-recursively) / base cases 
     let prev = 0;
-    let curr = 1;
-    //start at 2 because we already accounter for the first two numbers: 0, 1
-    for(let i = 2; i < n; i ++){ 
-        let newCurr = prev + curr;
-        console.log(`${prev} + ${curr} = ${newCurr}`)
-        prev = curr;
-        curr = newCurr;
+    let cur = 1;
+    for(let i = 2; i < n; i++){
+        let newPrev = cur;
+        let newCur = cur + prev;
+        cur = newCur;
+        prev = newPrev;
     }
-    return curr;
+    return cur;
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~2. filter unique chars
-//['banana', 'cat', 'apple', 'dog']
-const uniqueChars = function(arr){
-    return arr.filter((word) => {
-        //create empty object to keep track of count per character
-       //for loop through each character in word 
-            //if character exists as key, add 1 to value
-                //return false
-            //else create new key value pair 
-        //return true 
+const uniqueTest = ['banana', 'cat', 'apple', 'dog']
+//2. filter out strings with unique characters
+const uniqueChars = function(arr) {
+    return arr.filter((el) => {
+        //could use a set 
+        let dict = {}
+        for(let i = 0; i < el.length; i++){
+            if (dict.hasOwnProperty(el[i])){
+                return false //immediately exists callback and doesn't check rest of chars
+            } else {
+                dict[el[i]] = 1;
+            }
+        }
+        return true;
     })
 }
 
 
+
+
+
 /*
-Phase 1 Week 1 Day 3
-Given a 2D array of strings where the first element represents a color and the second element represents an association, create a dictionary of key/value pairs where the color is the key and the association is the value.
-Ex. 
-[["white", "flowers"], ["red", "blood"], ["yellow", "sun"], ["blue", "sky"], ["brown", "earth"], ["black", "night"], ["purple", "violet"], ["orange", "orange"]] =>
+Phase 2 Week 1 Day 3:
+1. Given a 2D array of strings where the first element represents a color and the second element represents an association, create a dictionary of key/value pairs where the color is the key and the association is the value.
+[["white", "flowers"], ["red", "blood"], ["yellow", "sun"], ["blue", "sky"], ["brown", "earth"], ["black", "night"], ["purple", "annoying people"], ["orange", "orange"]] =>
 {
-    white: "flowers",
+    white: "flowers", 
     red: "blood",
-    yellow: "sun",
-    blue: "sky",
-    brown: "earth",
-    black: "night",
-    purple: "violet",
+    yellow: "sun", 
+    blue: "sky", 
+    brown: "earth", 
+    black: "night", 
+    purple: "annoying people", 
     orange: "orange"
 }
+2. Given an array of strings write a function that removes all spaces in each string
+Ex. 
+['app le', 'se ri ous', 'fell ow ship', 'red bul l'] => ['apple', 'serious', 'fellowship', 'redbull']
 */
 
-const convertDictTest = [["white", "flowers"], ["red", "blood"], ["yellow", "sun"], ["blue", "sky"], ["brown", "earth"], ["black", "night"], ["purple", "violet"], ["orange", "orange"]]
-const convertDict = function(arr){
-    let newObj = {}
-    // arr.map((el) => {
-    //     newObj[el[0]] = el[1]
-    // })
-    //for each individual array inside
-    for(let i = 0; i < arr.length; i++){
-        //for each element inside current nested array
-        console.log(arr[i]);
-        for(let j = 0; j < arr[i].length; j++) {
-            console.log(arr[i][j])
-        }
-    }
-    return newObj;
+//1. association
+const colorAssTest = [["white", "flowers"], ["red", "blood"], ["yellow", "sun"], ["blue", "sky"], ["brown", "earth"], ["black", "night"], ["purple", "annoying people"], ["orange", "orange"]]
+const colorAssociation = function(arr) {
+    let dict = {};
+    arr.forEach((el) => {
+        dict[el[0]] = el[1]
+    })
+    return dict;
 }
 
+//2. remove white spaces
+const removeSpaceTest = ['app le', 'se ri ous', 'fell ow ship', 'red bul l'];
+const removeSpaces = function(arr){
+    return arr.map((el) => el.replaceAll(' ', ''))
+}
 
-console.log(convertDict(convertDictTest));
+/* Phase 2 Week 1 Day 4
+1. Given an integer n that is the square of some integer x, find the next perfect square.  You should be returning the square of x + 1.  If n is not a perfect square return -1. 
+    Ex. 121 => 144, 114 => -1, 625 => 676
+2. Given an array of unsorted integers, return the missing integer.
+	Ex. [1, 3, 6, 5, 2] => 4
+	    [5, 2, 4, 3] => 1
+*/
 
+//1. nextPerfectSquare
+const nextPerfectSquare = function(n){
+    let sqrt = Math.sqrt(n);
+    if(Math.floor(sqrt) !== sqrt){
+        return -1
+    }
+    return (sqrt + 1)**2;
+}
 
-
-
+//2. missing integer
+const missingIntTestOne = [1, 3, 6, 5, 2]
+const missingIntTestTwo = [5, 2, 4, 3]
+const missingInteger = function(arr){
+    for(let i = 1; i <= arr.length; i++){
+        if(!arr.includes(i)){
+            return i
+        }
+    }
+    return -1
+}
